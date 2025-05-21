1f9662f2e99895ec0064a5808b4977482599d1ce
Richard Albert Leddy <858872+rleddy@users.noreply.github.com>
2025-05-21 14:19:16 -0700

    Make an endpoint service rather than a web service

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const GeneralLinkServer = require('./lib/data_server_class')
+const MetaDataEndpointServer= require('./lib/data_endpoint_class')
 
 const RecordSearchApp = require('./lib/the_record_searcher_app')
 const ServiceEntryWatcher = require('./lib/app_dir_watcher')
 
 
 module.exports = GeneralLinkServer
+module.exports = MetaDataEndpointServer
 module.exports.RecordSearchApp = RecordSearchApp
 module.exports.ServiceEntryWatcher = ServiceEntryWatcher
